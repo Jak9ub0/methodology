8d3c8da16bce62ebd801727e5ef1ec083c6e4929
Artur Ciepielowski <artciepi@gmail.com>
2022-04-05 22:17:45 +0200

    Not sure if this is what author of the task had in mind

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -1,7 +1,27 @@
 package com.sda.arppl2;
 
 public class Main {
+
+    public static void name(String bearName) {
+        System.out.println("Imię misia: "+bearName);
+    }
+
+    public static void age(int bearAge) {
+        System.out.println("Wiek misia: "+bearAge);
+    }
     public static void main(String[] args) {
-        System.out.println("Zadanie 1 wykonane!");
+
+        Main Bear = new Main();
+        Bear.name("Marian");
+        Bear.age(32);
+
+        Bear.name("Olek");
+        Bear.age(2);
+
+        Bear.name("Filipek");
+        Bear.age(10);
+
+        Bear.name("Teddy");
+        Bear.age(3);
     }
 }
