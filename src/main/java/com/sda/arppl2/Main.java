6024f9d1280555369290c14df02e193d0916e301
Artur Ciepielowski <artciepi@gmail.com>
2022-04-06 19:07:16 +0200

    Solved the problem

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -2,26 +2,24 @@ package com.sda.arppl2;
 
 public class Main {
 
-    public static void name(String bearName) {
-        System.out.println("Imię misia: "+bearName);
-    }
 
-    public static void age(int bearAge) {
-        System.out.println("Wiek misia: "+bearAge);
-    }
     public static void main(String[] args) {
+        Bear p = new Bear("Marian",32);
+        System.out.println(p.name);
+        System.out.println(p.age);
+        Bear a = new Bear("Olek",2);
+        System.out.println(a.name);
+        System.out.println(a.age);
+        Bear b = new Bear("Filipek",10);
+        System.out.println(b.name);
+        System.out.println(b.age);
+        Bear c = new Bear("Teddy",3);
+        System.out.println(c.name);
+        System.out.println(c.age);
 
-        Main Bear = new Main();
-        Bear.name("Marian");
-        Bear.age(32);
-
-        Bear.name("Olek");
-        Bear.age(2);
-
-        Bear.name("Filipek");
-        Bear.age(10);
-
-        Bear.name("Teddy");
-        Bear.age(3);
     }
+
+
+
+
 }
